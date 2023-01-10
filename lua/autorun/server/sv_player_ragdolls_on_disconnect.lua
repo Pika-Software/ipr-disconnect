cfbf1bfce261a0cd8a9d71ebbf3dbc8d9cb621dc
PrikolMen <olegis019@gmail.com>
2023-01-10 19:34:19 +0400

    ts

--- lua/autorun/server/sv_player_ragdolls_on_disconnect.lua
+++ lua/autorun/server/sv_player_ragdolls_on_disconnect.lua
@@ -42,88 +42,85 @@ hook.Add('PlayerInitialSpawn', addonName, function( ply )
 	local steamID = getSteamID( ply )
 	for _, ent in ipairs( ents.GetAll() ) do
 		if ent:IsPlayerRagdoll() and ent.DisconnectedPlayer and (ent.SteamID == steamID) then
-			-- Model
-			ply:SetModel( ent:GetModel() )
-
-			-- Skin
-			ply:SetSkin( ent:GetSkin() )
-
-			-- Bodygroups
-			for _, bodygroup in ipairs( ent:GetBodyGroups() ) do
-				ply:SetBodygroup( bodygroup.id, ent:GetBodygroup( bodygroup.id ) )
-			end
-
-			-- Flexes
-			ply:SetFlexScale( ent:GetFlexScale() )
-			for flex = 1, ply:GetFlexNum() do
-				ply:SetFlexWeight( flex, ent:GetFlexWeight( flex ) )
-			end
-
-			-- Color & Material
-			ply:SetMaterial( ent:GetMaterial() )
-			ply:SetColor( ent:GetColor() )
-
-			-- Position & Angles
-			local pos = ent:LocalToWorld( ent:OBBCenter() )
-			local ang = ent.PlayerAngles
-
 			timer.Simple(0, function()
-				if IsValid( ply ) then
-					ply:SetEyeAngles( Angle( 0, ang[2], 0 ) )
-					ply:SetPos( pos )
+				-- Model
+				ply:SetModel( ent:GetModel() )
+
+				-- Skin
+				ply:SetSkin( ent:GetSkin() )
+
+				-- Bodygroups
+				for _, bodygroup in ipairs( ent:GetBodyGroups() ) do
+					ply:SetBodygroup( bodygroup.id, ent:GetBodygroup( bodygroup.id ) )
 				end
+
+				-- Flexes
+				ply:SetFlexScale( ent:GetFlexScale() )
+				for flex = 1, ply:GetFlexNum() do
+					ply:SetFlexWeight( flex, ent:GetFlexWeight( flex ) )
+				end
+
+				-- Color & Material
+				ply:SetMaterial( ent:GetMaterial() )
+				ply:SetColor( ent:GetColor() )
+
+				-- Position & Angles
+				ply:SetEyeAngles( Angle( 0, ent.PlayerAngles[2], 0 ) )
+				ply:SetPos( ent:LocalToWorld( ent:OBBCenter() ) )
+
+				-- Weapons
+				ply:StripWeapons()
+
+				for class, wep in pairs( ent.Weapons ) do
+					if not IsValid( wep ) then
+						wep = ents.Create( class )
+						if IsValid( wep ) then
+							wep:SetPos( pos )
+							wep:Spawn()
+						end
+					end
+
+					if IsValid( wep ) then
+						wep:SetParent()
+						wep:SetNoDraw( false )
+						ply:PickupWeapon( wep )
+						wep:SetCollisionGroup( COLLISION_GROUP_NONE )
+					end
+				end
+
+				-- Active Weapon
+				local activeWeapon = ent.ActiveWeapon
+				if not IsValid( activeWeapon ) then
+					local class = ent.ActiveWeaponClass
+					if isstring( class ) then
+						activeWeapon = ents.Create( class )
+						if IsValid( activeWeapon ) then
+							activeWeapon:SetPos( pos )
+							activeWeapon:Spawn()
+							ply:PickupWeapon( activeWeapon )
+						end
+					end
+				end
+
+				if IsValid( activeWeapon ) then
+					ply:SetActiveWeapon( activeWeapon )
+				end
+
+				-- Bone Manipulations
+				for i = 0, ply:GetBoneCount() do
+					ply:ManipulateBonePosition( i, ent:GetManipulateBonePosition( i ) )
+					ply:ManipulateBoneAngles( i, ent:GetManipulateBoneAngles( i ) )
+					ply:ManipulateBoneJiggle( i, ent:GetManipulateBoneJiggle( i ) )
+					ply:ManipulateBoneScale( i, ent:GetManipulateBoneScale( i ) )
+				end
+
+				-- Velocity
+				ply:SetVelocity( ent:GetVelocity() )
+
+				-- Removing
+				ent:Remove()
 			end)
 
-			-- Weapons
-			ply:StripWeapons()
-
-			for class, wep in pairs( ent.Weapons ) do
-				if not IsValid( wep ) then
-					wep = ents.Create( class )
-					if IsValid( wep ) then
-						wep:SetPos( pos )
-						wep:Spawn()
-					end
-				end
-
-				if IsValid( wep ) then
-					wep:SetParent()
-					wep:SetNoDraw( false )
-					ply:PickupWeapon( wep )
-					wep:SetCollisionGroup( COLLISION_GROUP_NONE )
-				end
-			end
-
-			-- Active Weapon
-			local activeWeapon = ent.ActiveWeapon
-			if not IsValid( activeWeapon ) then
-				local class = ent.ActiveWeaponClass
-				if isstring( class ) then
-					activeWeapon = ents.Create( class )
-					if IsValid( activeWeapon ) then
-						activeWeapon:SetPos( pos )
-						activeWeapon:Spawn()
-						ply:PickupWeapon( activeWeapon )
-					end
-				end
-			end
-
-			if IsValid( activeWeapon ) then
-				ply:SetActiveWeapon( activeWeapon )
-			end
-
-			-- Bone Manipulations
-			for i = 0, ply:GetBoneCount() do
-				ply:ManipulateBonePosition( i, ent:GetManipulateBonePosition( i ) )
-				ply:ManipulateBoneAngles( i, ent:GetManipulateBoneAngles( i ) )
-				ply:ManipulateBoneJiggle( i, ent:GetManipulateBoneJiggle( i ) )
-				ply:ManipulateBoneScale( i, ent:GetManipulateBoneScale( i ) )
-			end
-
-			-- Velocity
-			ply:SetVelocity( ent:GetVelocity() )
-
-			ent:Remove()
 			break
 		end
 	end
